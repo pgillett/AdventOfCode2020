ced5ec8f101584d4b4bcb0b04932952a8dbef741
Peter Gillett <peter@gillett.co.uk>
2020-12-02 06:32:57 +0000

    Day1

---
+++ Advent/Advent/Day1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Advent
+{
+    public class Day1
+    {
+        public (int, int) FindExpenseTwo(string allExpenses)
+        {
+            var expenses = allExpenses
+                .Split(Environment.NewLine)
+                .Select(s => int.Parse(s))
+                .ToList();
+
+            for(int first = 0; first <expenses.Count -1 ;first++)
+            {
+                for(int second = first+1; second<expenses.Count ;second++)
+                {
+                    if (expenses[first] + expenses[second] == 2020) return (expenses[first], expenses[second]);
+                }
+            }
+
+            throw new Exception("Not found");
+        }
+
+        public (int, int, int) FindExpenseThree(string allExpenses)
+        {
+            var expenses = allExpenses
+                .Split(Environment.NewLine)
+                .Select(s => int.Parse(s))
+                .ToList();
+
+            for (int first = 0; first < expenses.Count - 2; first++)
+            {
+                for (int second = first + 1; second < expenses.Count-1; second++)
+                {
+                    for (int third = second + 1; third < expenses.Count; third++)
+                    {
+                        if (expenses[first] + expenses[second] + expenses[third] == 2020)
+                            return (expenses[first], expenses[second], expenses[third]);
+                    }
+                }
+            }
+
+            throw new Exception("Not found");
+        }
+    }
+}
---
+++ Advent/Advent/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Advent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
