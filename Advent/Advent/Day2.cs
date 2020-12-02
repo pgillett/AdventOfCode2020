e5c2a2ddb8b662894f5728e5f9c90956237cf9d2
Peter Gillett <peter@gillett.co.uk>
2020-12-02 07:07:55 +0000

    Day2

---
+++ Advent/Advent/Day2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace Advent
+{
+    public class Day2
+    {
+        public int CountCorrectPasswordsNumber(string passwordList)
+        {
+            var passwordSets = passwordList.Split(Environment.NewLine);
+
+            return passwordSets.Count(p => IsValidNumber(p));
+        }
+
+        public int CountCorrectPasswordsPosition(string passwordList)
+        {
+            var passwordSets = passwordList.Split(Environment.NewLine);
+
+            return passwordSets.Count(p => IsValidPosition(p));
+        }
+
+        public bool IsValidNumber(string passwordSet)
+        {
+            SplitPassword(passwordSet, out var minimum, out var maximum,
+                out var character, out var password);
+
+            var count = password.Count(c => c == character);
+
+            return count >= minimum && count <= maximum;
+        }
+
+        public bool IsValidPosition(string passwordSet)
+        {
+            SplitPassword(passwordSet, out var position1, out var position2,
+                out var character, out var password);
+
+            return password[position1-1] == character ^ password[position2-1] == character;
+        }
+
+        public void SplitPassword(string passwordSet, out int value1, out int value2, 
+            out char character, out string password)
+        {
+            var split = passwordSet.Split('-', ' ', ':');
+
+            value1 = int.Parse(split[0]);
+            value2 = int.Parse(split[1]);
+
+            character = split[2][0];
+
+            password = split[4].Trim();
+        }
+    }
+}
