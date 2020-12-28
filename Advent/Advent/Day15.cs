30518eae4670e73177ec717a7fa1cffd977d4727
Peter Gillett <peter@gillett.co.uk>
2020-12-28 13:37:49 +0000

    Day15 refactor for speed

--- Advent/Advent/Day15.cs
+++ Advent/Advent/Day15.cs
@@ -5,11 +5,13 @@ namespace Advent
 {
     public class Day15
     {
-        public int Find(int toTurn, string input)
+        public int FindDictionary(int toTurn, string input)
         {
+            // Original solution
+            
             var values = input
                 .Split(',')
-                .Select(l => int.Parse(l))
+                .Select(int.Parse)
                 .ToArray();
 
             var position = new Dictionary<int, int>();
@@ -22,7 +24,45 @@ namespace Advent
 
             while (currentPosition < toTurn - 1)
             {
-                if (position.ContainsKey(lastSpoken))
+                if (position.TryGetValue(lastSpoken, out var lastPosition))
+                {
+                    position[lastSpoken] = currentPosition;
+                    lastSpoken = currentPosition - lastPosition;
+                }
+                else
+                {
+                    position[lastSpoken] = currentPosition;
+                    lastSpoken = 0;
+                }
+
+                currentPosition++;
+            }
+
+            return lastSpoken;
+        }
+        
+        public int Find(int toTurn, string input)
+        {
+            // Quicker. Appears to also be more memory efficient even with the big array
+            
+            var values = input
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+
+            var position = new int[toTurn];
+            for (var i = 0; i < position.Length; i++)
+                position[i] = -1;
+
+            for (var i = 0; i < values.Length - 1; i++)
+                position[values[i]] = i;
+
+            var currentPosition = values.Length - 1;
+            var lastSpoken = values.Last();
+
+            while (currentPosition < toTurn - 1)
+            {
+                if (position[lastSpoken]>=0)
                 {
                     var lastPosition = position[lastSpoken];
                     position[lastSpoken] = currentPosition;
